e0251732846da5dad51ecd351823ddb094dddf57
Toby <tobyyegian@berkeley.edu>
2025-05-15 17:46:19 -0700

    deleted lot of file cruft

--- input_models/a.lua
+++ input_models/a.lua
@@ -1,6 +1,6 @@
 -- Lua script.
 p=tetview:new()
-p:load_mesh("C:/Users/Californication/Desktop/Junior/C106B/Final_Project/nonplanar-infill/S4_Slicer/input_models/tee_ascii.1.face")
+p:load_mesh("C:/Users/Californication/Desktop/Junior/C106B/Final_Project/nonplanar-infill/S4_Slicer/_skipped.face")
 rnd=glvCreate(0, 0, 500, 500, "TetView")
 p:plot(rnd)
 glvWait()
